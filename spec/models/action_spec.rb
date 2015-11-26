0f9016c30b01a8b1f6af0f0fd3f7014e3846fb73
Jono Mingard <reason.koan@gmail.com>
2015-11-26 13:57:33 +1300

    Add spec for event and action validations

--- spec/models/action_spec.rb
+++ spec/models/action_spec.rb
@@ -2,4 +2,42 @@ require 'rails_helper'
 
 RSpec.describe Action, type: :model do
   let(:action) { create(:action) }
+
+  describe "#event" do
+    it "is required" do
+      action.event = nil
+      expect(action).not_to be_valid
+    end
+  end
+
+  describe "#territory" do
+    it "is required" do
+      action.territory = nil
+      expect(action).not_to be_valid
+    end
+  end
+
+  describe "#territory_owner" do
+    it "is required" do
+      action.territory_owner = nil
+      expect(action).not_to be_valid
+    end
+  end
+
+  describe "#units_difference" do
+    it "can be positive" do
+      action.units_difference = 10
+      expect(action).to be_valid
+    end
+
+    it "can be negative" do
+      action.units_difference = -10
+      expect(action).to be_valid
+    end
+
+    it "cannot be zero" do
+      action.units_difference = 0
+      expect(action).not_to be_valid
+    end
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:event) { create(:event) }
+
+  describe "#event_type" do
+    %i{reinforce attack fortify end_turn}.each do |type|
+      it "can be #{type}" do
+        event.event_type = type
+        expect(event).to be_valid
+      end
+    end
+
+    it "cannot be an invalid type" do
+      expect { event.event_type = :nothing }.to raise_error ArgumentError
+    end
+  end
+
+  describe "#player" do
+    it "is required" do
+      event.player = nil
+      expect(event).not_to be_valid
+    end
+  end
 end
