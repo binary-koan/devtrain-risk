0988a4fef98b1ea8543d5c8c42cd2a56df9ebed5
Jono Mingard <reason.koan@gmail.com>
2015-11-27 16:25:32 +1300

    Add spec for CreateGame service

--- app/concepts/game_state.rb
+++ app/concepts/game_state.rb
@@ -5,11 +5,17 @@ class GameState
     @game = game
   end
 
-  def units_on_territory(territory)
-    Action.where(territory: territory).inject(0) { |total, action| total + action.units_difference }
+  def owned_territories(player)
+    game.territories.select do |territory|
+      territory_owner(territory) == player
+    end
   end
 
   def territory_owner(territory)
     Action.where(territory: territory).last.territory_owner
+  end
+
+  def units_on_territory(territory)
+    Action.where(territory: territory).inject(0) { |total, action| total + action.units_difference }
   end
 end
--- spec/services/create_game_spec.rb
+++ spec/services/create_game_spec.rb
@@ -3,6 +3,39 @@ require "spec_helper"
 RSpec.describe CreateGame do
   describe "#call" do
     let(:service) { CreateGame.new }
-    subject(:result) { service.call }
+
+    subject(:game) { service.call }
+    let(:game_state) { GameState.new(game) }
+
+    it "returns a saved game instance" do
+      expect(game).to be_a Game
+      expect(game).to be_persisted
+    end
+
+    it "creates players for the game" do
+      expect(game.players.size).to eq 2
+    end
+
+    it "assigns names to the players" do
+      expect(game.players[0].name).to eq "Player 1"
+      expect(game.players[1].name).to eq "Player 2"
+    end
+
+    it "creates territories in the game" do
+      expect(game.territories.size).to eq 6
+    end
+
+    it "assigns players to territories" do
+      player1_territories = game_state.owned_territories(game.players[0])
+      expect(player1_territories.size).to eq 3
+      player2_territories = game_state.owned_territories(game.players[1])
+      expect(player2_territories.size).to eq 3
+    end
+
+    it "adds units to territories" do
+      game.territories.each do |territory|
+        expect(game_state.units_on_territory(territory)).to eq 10
+      end
+    end
   end
 end
