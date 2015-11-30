fe6178af0f078990bba3f0c09f6296f3913df4d3
Jono Mingard <reason.koan@gmail.com>
2015-12-01 09:40:28 +1300

    Add end turn service

---
+++ app/services/end_turn.rb
@@ -0,0 +1,14 @@
+class EndTurn
+  def initialize(game)
+    @game = game
+    @game_state = GameState.new(game)
+  end
+
+  def call
+    current_player = @game_state.current_player
+    next_player_index = @game.players.find_index(current_player) + 1
+    next_player_index = 0 if next_player_index == @game.players.size
+
+    @game.events.create!(player: @game.players[next_player_index], event_type: :start_turn)
+  end
+end
