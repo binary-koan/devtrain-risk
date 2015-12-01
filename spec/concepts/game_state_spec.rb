936bfb41fd3092354ee113ce2010965ca5777dc5
Jono Mingard <reason.koan@gmail.com>
2015-12-01 14:24:37 +1300

    Add spec for owned territories

--- spec/concepts/game_state_spec.rb
+++ spec/concepts/game_state_spec.rb
@@ -1,36 +1,75 @@
 require "rails_helper"
 
 RSpec.describe GameState do
-  let(:game) { CreateGame.new.call }
+  let(:game)    { create(:game) }
+  let(:player1) { create(:player, game: game) }
+  let(:player2) { create(:player, game: game) }
+  let(:jupiter) { create(:territory, game: game) }
+  let(:mars)    { create(:territory, game: game) }
+
   subject(:game_state) { GameState.new(game) }
 
   describe "#current_player" do
     subject { game_state.current_player }
+    before { create(:start_turn_event, game: game, player: player1) }
 
-    context "at the start of the game" do
-      it { is_expected.to eq game.players.first }
+    context "when one player has started their turn" do
+      it { is_expected.to eq player1 }
     end
 
-    context "when player 1 has ended their turn" do
-      before { EndTurn.new(game).call }
+    context "when the other player has also started their turn" do
+      before { create(:start_turn_event, game: game, player: player2) }
 
-      it { is_expected.to eq game.players[1] }
-    end
-
-    context "when both players have ended their turns" do
-      before { 2.times { EndTurn.new(game).call } }
-
-      it { is_expected.to eq game.players.first }
+      it { is_expected.to eq player2 }
     end
   end
 
   describe "#owned_territories" do
-    context "at the start of the game" do
-      it "is the same number for both players" do
-        player1_territories = game_state.owned_territories(game.players.first)
-        player2_territories = game_state.owned_territories(game.players.last)
+    context "with no territories owned" do
+      it "is empty for both players" do
+        expect(game_state.owned_territories(player1)).to be_empty
+        expect(game_state.owned_territories(player2)).to be_empty
+      end
+    end
 
-        expect(player1_territories.size).to eq player2_territories.size
+    context "when one player owns one territory" do
+      before { create(:reinforce_event, player: player1, game: game, territory: jupiter) }
+
+      it "contains the territory for the owning player" do
+        expect(game_state.owned_territories(player1)).to contain_exactly(jupiter)
+      end
+
+      it "is still empty for the other player" do
+        expect(game_state.owned_territories(player2)).to be_empty
+      end
+    end
+
+    context "when both players own one territory" do
+      before do
+        create(:reinforce_event, player: player1, game: game, territory: mars)
+        create(:reinforce_event, player: player2, game: game, territory: jupiter)
+      end
+
+      it "contains the correct territory for player 1" do
+        expect(game_state.owned_territories(player1)).to contain_exactly(mars)
+      end
+
+      it "contains the correct territory for player 2" do
+        expect(game_state.owned_territories(player2)).to contain_exactly(jupiter)
+      end
+
+      context "when one player has taken over the other one's territory" do
+        before do
+          create(:takeover_event, player: player1, game: game, territory: jupiter)
+        end
+
+        it "contains both territories for player 1" do
+          expect(game_state.owned_territories(player1)).to contain_exactly(jupiter, mars)
+        end
+
+        it "is empty for player 2" do
+          expect(game_state.owned_territories(player2)).to be_empty
+        end
       end
     end
   end
--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,7 +1,22 @@
 FactoryGirl.define do
-  factory :event do
+  factory :event, aliases: [:start_turn_event] do
     game
     player
     event_type "start_turn"
+
+    factory :reinforce_event, traits: [:assigns_units_to_territory] do
+      event_type "reinforce"
+    end
+
+    factory :takeover_event, traits: [:assigns_units_to_territory] do
+      event_type "attack"
+    end
+
+    trait(:assigns_units_to_territory) do
+      transient { territory(nil) }
+      after(:create) do |e, attrs|
+        e.actions << create(:action, territory_owner: e.player, territory: attrs.territory)
+      end
+    end
   end
 end
