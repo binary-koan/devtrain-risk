d3ee5ddd11c42917f702289531a702d82818d9e7
Jono Mingard <reason.koan@gmail.com>
2015-12-03 15:55:52 +1300

    Remove old games controller actions

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,36 +15,4 @@ class GamesController < ApplicationController
   def show
     @game_state = GameState.current(@game)
   end
-
-  def event
-    handle_event
-    redirect_to @game
-  end
-
-  def end_turn
-    EndTurn.new(@game).call
-    redirect_to @game
-  end
-
-  private
-
-  def assign_game
-    @game = Game.find(params[:id])
-  end
-
-  def handle_event
-    service = PerformAttack.new(attack_params)
-
-    unless service.call
-      flash.alert = service.errors
-    end
-  end
-
-  def attack_params
-    {
-      territory_from: @game.territories[params[:from].to_i],
-      territory_to: @game.territories[params[:to].to_i],
-      game_state: GameState.new(@game)
-    }
-  end
 end
