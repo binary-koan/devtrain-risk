3b0492f5d3873d4e12dcdf6ab4c8e82f8d76c467
Marcel van Workum <Marcel.vanworkum@gmail.com>
2015-12-04 14:19:03 +1300

    implemented specs for events controller

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
-  pending "Test it!"
+  fixtures :games
+
+  describe "#create" do
+    let(:call_result) { true }
+    let(:submit_event) { instance_double(SubmitEvent, call: call_result) }
+
+    it "redirects to the game" do
+      expect(SubmitEvent).to receive(:new).and_return submit_event
+      post :create, game_id: games(:game).id
+      expect(response).to redirect_to game_path(Game.last)
+    end
+
+    context "the submit returns an error" do
+      let(:call_result) { false }
+
+      it "adds an error to the flash" do
+        expect(SubmitEvent).to receive(:new).and_return submit_event
+        expect(submit_event).to receive(:errors).and_return [:unknown_event_type]
+        post :create, game_id: games(:game).id
+        expect(flash.alert).to eq [:unknown_event_type]
+      end
+    end
+  end
 end
