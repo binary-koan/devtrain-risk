df732cafc35c3e0f24899a1f9c327bdb13490bb0
Jono Mingard <reason.koan@gmail.com>
2015-12-08 13:23:04 +1300

    Refactor build turn service into separate methods

--- app/services/build_turn.rb
+++ app/services/build_turn.rb
@@ -1,23 +1,31 @@
 class BuildTurn
   def initialize(events)
     @events = events
+    @turns = []
   end
 
   def call
-    start_turn_indexes = @events.each.with_index
-      .select { |event, i| event.start_turn? }
-      .map { |event, i| i }
-    start_turn_indexes = [0, *start_turn_indexes, @events.length]
+    all_turns.last
+  end
 
-    turn_groups = start_turn_indexes.each_cons(2)
-      .map { |start, finish| @events[start...finish] }
+  private
 
+  def all_turns
     current_turn = nil
-    turn_groups.each do |events|
-      next if events.empty? #TODO why is something empty?
+    turn_groups.map do |events|
       current_turn = Turn.new(events, current_turn)
     end
+  end
 
-    current_turn
+  def turn_groups
+    [0, *start_turn_indexes, @events.length].each_cons(2).map do |start_index, next_index|
+      @events[start_index..next_index]
+    end
+  end
+
+  def start_turn_indexes
+    @events.each.with_index
+      .select { |event, i| event.start_turn? }
+      .map { |event, i| i }
   end
 end
