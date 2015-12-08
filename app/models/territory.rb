1c3fa4c2abd6adae09760dab70d0054267784f71
Jono Mingard <reason.koan@gmail.com>
2015-12-09 10:39:40 +1300

    Use has_many associations for territory links

--- app/models/territory.rb
+++ app/models/territory.rb
@@ -2,22 +2,15 @@ class Territory < ActiveRecord::Base
   belongs_to :game
   has_many :actions, dependent: :destroy
 
-  # has_many :from_territory_links, class_name: "TerritoryLink", foreign_key: "from_territory_id"
-  # has_many :from_territories, through: :from_territory_links, source: :from_territory
-  # has_many :to_territory_links, class_name: "TerritoryLink", foreign_key: "to_territory_id"
-  # has_many :to_territories, through: :to_territory_links, source: :to_territory
-  #
-  # def connected_territories
-  #   from_territories + to_territories
-  # end
+  has_many :to_territory_links, class_name: "TerritoryLink", foreign_key: "to_territory_id"
+  has_many :from_territories, through: :to_territory_links, source: :from_territory
+
+  has_many :from_territory_links, class_name: "TerritoryLink", foreign_key: "from_territory_id"
+  has_many :to_territories, through: :from_territory_links, source: :to_territory
+
+  def connected_territories
+    from_territories + to_territories
+  end
 
   validates :game, :name, presence: true
-
-  # TODO moar has many through
-  def connected_territories
-    links_to = TerritoryLink.where(from_territory: self).pluck(:to_territory_id)
-    links_from = TerritoryLink.where(to_territory: self).pluck(:from_territory_id)
-
-    Territory.find(links_to + links_from)
-  end
 end
--- spec/models/territory_spec.rb
+++ spec/models/territory_spec.rb
@@ -1,12 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Territory, type: :model do
-  let(:territory) { create(:territory) }
+  let(:territory) { create(:territory, name: "Jupiter") }
 
   describe "#game" do
     it "is required" do
       territory.game = nil
       expect(territory).to_not be_valid
+    end
+  end
+
+  describe "#connected_territories" do
+    let(:second_territory) { create(:territory, name: "Mars") }
+    let(:third_territory) { create(:territory, name: "Saturn") }
+
+    context "with a link from this territory" do
+      before do
+        create(:territory_link, from_territory: territory, to_territory: second_territory)
+      end
+
+      it "contains the linked territory" do
+        expect(territory.connected_territories).to contain_exactly(second_territory)
+      end
+    end
+
+    context "with a link to this territory" do
+      before do
+        create(:territory_link, from_territory: territory, to_territory: second_territory)
+      end
+
+      it "contains the linked territory" do
+        expect(territory.connected_territories).to contain_exactly second_territory
+      end
+    end
+
+    context "with links to and from this territory" do
+      before do
+        create(:territory_link, from_territory: territory, to_territory: second_territory)
+        create(:territory_link, from_territory: third_territory, to_territory: territory)
+      end
+
+      it "contains both linked territories" do
+        expect(territory.connected_territories).to contain_exactly(second_territory, third_territory)
+      end
+    end
+
+    context "with a directly and indirectly linked territory" do
+      before do
+        create(:territory_link, from_territory: territory, to_territory: second_territory)
+        create(:territory_link, from_territory: second_territory, to_territory: third_territory)
+      end
+
+      it "only contains the directly linked territory" do
+        expect(territory.connected_territories).to contain_exactly(second_territory)
+      end
     end
   end
 end
