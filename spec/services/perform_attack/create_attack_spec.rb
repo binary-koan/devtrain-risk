442d299212df12a5fc7f5b56c3919a29f8be439e
Marcel van Workum <Marcel.vanworkum@gmail.com>
2015-12-10 12:52:45 +1300

    created specs for multiple attacks

--- spec/services/perform_attack/create_attack_spec.rb
+++ spec/services/perform_attack/create_attack_spec.rb
@@ -148,13 +148,61 @@ RSpec.describe PerformAttack::CreateAttack do
       let(:attacking_units) { 4 }
 
       before do
-        2.times { expect(PerformAttack::RollDice).to receive(:new).and_return -> { [[3, 3], [2, 2]] } }
+        expect(PerformAttack::RollDice).to receive(:new).exactly(:twice).and_return -> { [[1, 6], [1, 6]] }
+        expect(PerformAttack::RollDice).to receive(:new).and_return -> { [[1, 6]] }
       end
 
-      let(:attack_actions) { attack_events.flat_map(&:actions) }
+      let(:first_event) { attack_events[0] }
+      let(:second_event) { attack_events[1] }
+      let(:third_event) { attack_events[2] }
 
-      it "takes over the territory" do
-        pending "IMPLEMENT"
+      it "kills 2 defenders in the first event" do
+        expect(first_event.actions.length).to eq 1
+        action = first_event.actions[0]
+        expect(action.action_type).to eq "kill"
+        expect(action.territory).to be territory_to
+        expect(action.territory_owner).to eq players(:player2)
+        expect(action.units_difference).to be -2
+      end
+
+      it "kills 2 defenders in the second event" do
+        expect(second_event.actions.length).to eq 1
+        action = second_event.actions[0]
+        expect(action.action_type).to eq "kill"
+        expect(action.territory).to be territory_to
+        expect(action.territory_owner).to eq players(:player2)
+        expect(action.units_difference).to be -2
+      end
+
+      context "killing the last defend and takes over the territory" do
+        it "contains the correct number of action" do
+          expect(third_event.actions.length).to eq 3
+        end
+
+        let(:kill_action) { third_event.actions[0] }
+        let(:move_to_action) { third_event.actions[1] }
+        let(:move_from_action) { third_event.actions[2] }
+
+        it "kills the last defender" do
+          expect(kill_action.action_type).to eq "kill"
+          expect(kill_action.territory).to be territory_to
+          expect(kill_action.territory_owner).to eq players(:player2)
+          expect(kill_action.units_difference).to be -1
+        end
+
+        it "moves the attackers to the territory" do
+          expect(move_to_action.action_type).to eq "move_to"
+          expect(move_to_action.territory).to be territory_to
+          expect(move_to_action.territory_owner).to eq players(:player1)
+          expect(move_to_action.units_difference).to be 4
+        end
+
+        it "removes the attackers from the attacking territory" do
+          expect(move_from_action.action_type).to eq "move_from"
+          expect(move_from_action.territory).to be territory_from
+          expect(move_from_action.territory_owner).to eq players(:player1)
+          expect(move_from_action.units_difference).to be -4
+        end
       end
     end
   end
