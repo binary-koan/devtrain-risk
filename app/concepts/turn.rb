dc25717045f5b38e06716525634bc881ae541394
Jono Mingard <reason.koan@gmail.com>
2015-12-10 11:53:19 +1300

    Allow free fortify event after taking over territory
    When you have taken over a territory, you can now fortify once from the
    attacking territory to the taken one without it ending your turn

--- app/concepts/turn.rb
+++ app/concepts/turn.rb
@@ -14,7 +14,10 @@ class Turn
     @phase = PHASE_REINFORCING
     @fortified = false
 
-    @events.each { |event| apply_event(event) }
+    @events.inject(nil) do |previous_event, event|
+      apply_event(event, previous_event)
+      event
+    end
   end
 
   def ==(other)
@@ -49,20 +52,39 @@ class Turn
 
   def actions
     previous_actions = @previous_turn ? @previous_turn.actions : []
-    previous_actions + @events.map(&:actions).flatten
+    previous_actions + @events.flat_map(&:actions)
   end
 
   private
 
-  def apply_event(event)
+  def apply_event(event, previous_event)
     if event.reinforce?
       reinforcements.remove(event.actions.first.units_difference)
       @phase = PHASE_ATTACKING if reinforcements.none?
     elsif event.attack?
       @phase = PHASE_ATTACKING
-    elsif event.fortify?
+    elsif event.fortify? && !fortifying_attacked_territory?(event, previous_event)
       @fortified = true
       @phase = PHASE_ENDING
     end
+  end
+
+  def fortifying_attacked_territory?(fortify_event, attack_event)
+    return unless territory_taken?(attack_event)
+
+    source_territory(fortify_event) == source_territory(attack_event) &&
+      target_territory(fortify_event) == target_territory(attack_event)
+  end
+
+  def territory_taken?(event)
+    event.attack? && !event.actions.move_to.empty?
+  end
+
+  def source_territory(event)
+    event.actions.move_from.first.territory
+  end
+
+  def target_territory(event)
+    event.actions.move_to.first.territory
   end
 end
--- app/models/action.rb
+++ app/models/action.rb
@@ -3,6 +3,8 @@ class Action < ActiveRecord::Base
   belongs_to :territory
   belongs_to :territory_owner, class_name: "Player"
 
-  validates :event, :territory, :territory_owner, presence: true
+  enum action_type: %i{add kill move_from move_to}
+
+  validates :event, :territory, :territory_owner, :action_type, presence: true
   validates :units_difference, exclusion: { in: [0], message: "can't be zero" }
 end
