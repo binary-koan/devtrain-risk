1592c9a4f107230be48afb6800776816c2de4009
Jono Mingard <reason.koan@gmail.com>
2015-12-11 16:06:21 +1300

    Add spec for map display helper

--- spec/helpers/games_helper_spec.rb
+++ spec/helpers/games_helper_spec.rb
@@ -10,4 +10,28 @@ RSpec.describe GamesHelper, type: :helper do
       expect(player1_color).not_to eq player2_color
     end
   end
+
+  describe "#map_display" do
+    let(:turn) { BuildTurn.new(games(:game).events).call }
+
+    let(:svg) { Nokogiri::XML(map_display(turn)) }
+
+    it "renders lines for links between territories" do
+      expect(svg.css("line").size).to eq 4
+    end
+
+    it "renders circles for territories" do
+      expect(svg.css("circle").size).to eq 4
+    end
+
+    it "renders territory names" do
+      ["Jupiter", "Mars", "Saturn", "Mercury"].each do |territory|
+        expect(svg.search("[text()='#{territory}']").size).to eq 1
+      end
+    end
+
+    it "renders the number of units on territories" do
+      expect(svg.search("[text()='5 units']").size).to eq 4
+    end
+  end
 end
