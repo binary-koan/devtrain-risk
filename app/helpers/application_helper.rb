7afd896e5fb9e85c6ef1735ae34bb5621e3ab853
Jono Mingard <reason.koan@gmail.com>
2015-12-15 10:16:22 +1300

    Handle icons on the server rather than client

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 module ApplicationHelper
-  def inline_svg(path)
-    File.open("app/assets/images/#{path}", "rb") do |file|
+  def icon_sprite
+    File.open("app/assets/images/icons.svg", "rb") do |file|
       raw file.read
     end
+  end
+
+  def icon(name)
+    content_tag("svg", content_tag("use", "", "xlink:href" => "#icon-#{name}"),
+      class: "icon #{name}")
   end
 end
