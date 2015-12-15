39ece2db9052af146322632874f1cc5d1da374f1
Jono Mingard <reason.koan@gmail.com>
2015-12-16 10:08:21 +1300

    Test ending a turn with a player who has lost

--- spec/services/end_turn_spec.rb
+++ spec/services/end_turn_spec.rb
@@ -13,18 +13,18 @@ RSpec.describe EndTurn do
       instance_double(Turn,
         game: games(:game),
         player: players(:player1),
-        game_state: game_state
+        game_state: game_state,
+        can_end_turn?: true
       )
     end
 
     subject(:service) { EndTurn.new(turn) }
 
     context "when the turn can be ended" do
-      before { expect(turn).to receive(:can_end_turn?).and_return(true) }
-
       it "succeeds and adds a start turn event to the game" do
         expect(service.call).to eq true
         expect(game.events.last.event_type).to eq "start_turn"
+        expect(game.events.last.player).to eq players(:player2)
       end
     end
 
@@ -34,6 +34,16 @@ RSpec.describe EndTurn do
       it "fails with an error" do
         expect(service.call).to eq false
         expect(service.errors).to contain_exactly :wrong_phase
+      end
+    end
+
+    context "when a player is out of the game" do
+      before { expect(game_state).to receive(:in_game?).and_return(false, true) }
+
+      it "skips the player who is out and starts the next player's turn" do
+        expect(service.call).to eq true
+        expect(game.events.last.event_type).to eq "start_turn"
+        expect(game.events.last.player).to eq players(:player3)
       end
     end
   end
